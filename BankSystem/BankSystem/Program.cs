66445b31a03d8d43f7fd0abc841d73893eb46084
Irina <irina.formagey@gmail.com>
2021-08-22 14:52:45 +0300

    Worked with reflection

--- BankSystem/BankSystem/Program.cs
+++ BankSystem/BankSystem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Bank_Sistem.Services;
 using BankSystem.Models;
 using BankSystem.Services;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -14,38 +15,50 @@ namespace BankSystem
             var clients = new BankServices<Client> { clientList = new List<Client>() };
             var fakeDataService = new FakeDataService();
 
-            //заполнение коллекций
-            while (clients.clientList.Count <= 10000)
-            {
-                var newClient = fakeDataService.GenerateClient();
-                clients.Add(newClient);
-            }
-            while (employees.employeeList.Count <= 1000)
-            {
-                var newEmployee = fakeDataService.GenerateEmployee();
-                employees.Add(newEmployee);
-            }
+            ////заполнение коллекций
+            //while (clients.clientList.Count <= 10000)
+            //{
+            //    var newClient = fakeDataService.GenerateClient();
+            //    clients.Add(newClient);
+            //}
+            //while (employees.employeeList.Count <= 1000)
+            //{
+            //    var newEmployee = fakeDataService.GenerateEmployee();
+            //    employees.Add(newEmployee);
+            //}
 
-            //заполнение словаря
-            var client1 = fakeDataService.GenerateClient();
-            var client2 = fakeDataService.GenerateClient();
+            ////заполнение словаря
+            //var client1 = fakeDataService.GenerateClient();
+            //var client2 = fakeDataService.GenerateClient();
 
-            var account1 = new Account { CurrencyType = new Rup(), CurrencyAmount = 10000 };
-            var account2 = new Account { CurrencyType = new Eur(), CurrencyAmount = 15 };
-            var account3 = new Account { CurrencyType = new Mdl(), CurrencyAmount = 1000 };
+            //var account1 = new Account { CurrencyType = new Rup(), CurrencyAmount = 10000 };
+            //var account2 = new Account { CurrencyType = new Eur(), CurrencyAmount = 15 };
+            //var account3 = new Account { CurrencyType = new Mdl(), CurrencyAmount = 1000 };
 
-            var dict = new Dictionary<string, List<Account>>
-            {
-                { client1.PassportId, new List<Account> { account1, account2 } },
-                { client2.PassportId, new List<Account> { account3 } }
-            };
+            //var dict = new Dictionary<string, List<Account>>
+            //{
+            //    { client1.PassportId, new List<Account> { account1, account2 } },
+            //    { client2.PassportId, new List<Account> { account3 } }
+            //};
 
-            //словарь в файл и обратно
-            var dictPath = Path.Combine("C:", "Users", "Irina", "source", "repos", "DexPractice",
-                "BankSystem", "BankSystem", "FileFolder", "ClientsAndAccountsDictionary.txt");
+            ////словарь в файл и обратно
+            //var dictPath = Path.Combine("C:", "Users", "Irina", "source", "repos", "DexPractice",
+            //    "BankSystem", "BankSystem", "FileFolder", "ClientsAndAccountsDictionary.txt");
 
-            clients.AddDictionaryToFile(dict, dictPath);
-            var desDict = clients.GetDictionaryFromFile(dictPath);
+            //clients.AddDictionaryToFile(dict, dictPath);
+            //var desDict = clients.GetDictionaryFromFile(dictPath);
+
+            //вывод свойств и значений объекта
+            var employee = fakeDataService.GenerateEmployee();
+            var client = fakeDataService.GenerateClient();
+            var account = new Account() { CurrencyType = new Rup(), CurrencyAmount = 10 };
+
+            var expData = new ExportData();
+            expData.AddDataToFile<Employee>(employee);
+            expData.AddDataToFile<Client>(client);
+            expData.AddDataToFile<Account>(account);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ BankSystem/BankSystem/Services/ExportData.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Bank_Sistem.Services
+{
+    public class ExportData
+    {
+        public static string path = Path.Combine("C:", "Users", "Irina", "source",
+          "repos", "DexPractice", "FileFolder");
+
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+        public void AddDataToFile<T>(T obj)
+        {
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+
+            var myType = obj.GetType();
+            var properties = myType.GetProperties();
+
+            var str = "";
+            foreach(var property in properties)
+            {
+                str += $"{property.Name}: {property.GetValue(obj)},  ";
+            }
+
+            using (var streamWriter = new FileStream($"{path}\\Report.txt", FileMode.Append))
+            {
+                var data = JsonConvert.SerializeObject(str);
+                byte[] dataArray = System.Text.Encoding.Default.GetBytes(data);
+                streamWriter.Write(dataArray);
+            }
+        }
+    }
+}
